a532add70a7c4d33ceeb57ebc3ac9436c9cf1b92
carlruan <62496966+carlruan@users.noreply.github.com>
2021-09-16 13:38:31 -0400

    Add files via upload

---
+++ src/main/java/edu/neu/coe/info6205/assignments/randomwalk/RandomWalkAssignment.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2017. Phasmid Software
+ */
+
+package edu.neu.coe.info6205.assignments.randomwalk;
+
+import edu.neu.coe.info6205.randomwalk.RandomWalk;
+
+import java.sql.SQLOutput;
+import java.util.Random;
+
+public class RandomWalkAssignment {
+
+    private int x = 0;
+    private int y = 0;
+
+    private final Random random = new Random();
+
+    /**
+     * Private method to move the current position, that's to say the drunkard moves
+     *
+     * @param dx the distance he moves in the x direction
+     * @param dy the distance he moves in the y direction
+     */
+    private void move(int dx, int dy) {
+        x += dx;
+        y += dy;
+        //System.out.println("curr positon: "+ x +", "+ y);
+        // TO BE IMPLEMENTED
+    }
+
+    /**
+     * Perform a random walk of m steps
+     *
+     * @param m the number of steps the drunkard takes
+     */
+    private void randomWalk(int m) {
+        while(m > 0){
+            randomMove();
+            m--;
+        }
+        // TO BE IMPLEMENTED
+    }
+
+    /**
+     * Private method to generate a random move according to the rules of the situation.
+     * That's to say, moves can be (+-1, 0) or (0, +-1).
+     */
+    private void randomMove() {
+        boolean ns = random.nextBoolean();
+        int step = random.nextBoolean() ? 1 : -1;
+        move(ns ? step : 0, ns ? 0 : step);
+    }
+
+    /**
+     * Method to compute the distance from the origin (the lamp-post where the drunkard starts) to his current position.
+     *
+     * @return the (Euclidean) distance from the origin to the current position.
+     */
+    public double distance() {
+        // TO BE IMPLEMENTED
+        return Math.sqrt(x * x + y * y);
+    }
+
+    /**
+     * Perform multiple random walk experiments, returning the mean distance.
+     *
+     * @param m the number of steps for each experiment
+     * @param n the number of experiments to run
+     * @return the mean distance
+     */
+    public static double randomWalkMulti(int m, int n) {
+        double totalDistance = 0;
+        for (int i = 0; i < n; i++) {
+            RandomWalk walk = new RandomWalk();
+            walk.randomWalk(m);
+            //System.out.println("no: " +i+" result : " + walk.distance());
+            totalDistance = totalDistance + walk.distance();
+        }
+        return totalDistance / n;
+    }
+
+
+    public static void main(String[] args) {
+        /*if (args.length == 0)
+            throw new RuntimeException("Syntax: RandomWalk steps [experiments]");
+        int m = Integer.parseInt(args[0]);
+        int m = 20;
+        int n = 6;
+        if (args.length > 1) n = Integer.parseInt(args[1]);*/
+
+        for(int i = 1; i <= 6; i++){
+            int m = (int)(Math.random()*100);
+            int n = (int)(Math.random()*100) + 10;
+            System.out.println("No "+ i );
+            double meanDistance = randomWalkMulti(m, n * 100000);
+            System.out.println(m + " steps: " + meanDistance + " over " + n * 100000 + " experiments");
+            System.out.println("------------------------------------------------------------");
+
+        }
+        /*while(n > 0){
+
+            System.out.println();
+            double meanDistance = randomWalkMulti(m, n);
+            System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments");
+        }*/
+
+    }
+
+}
